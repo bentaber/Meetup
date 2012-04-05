86f05dd0e71775978e13b30d6d4a99ea9aaed016
Ben Taber <ben.taber@gmail.com>
2012-04-05 09:14:04 -0600

    better

--- Content/js/angrierlib.js
+++ Content/js/angrierlib.js
@@ -3,38 +3,36 @@
 	"use strict";
 
 	var $angrylib = $('#angrierLibContent');
-	var images = [], movin = false, idx = -1;
+	var images = [], movin = false;
 
 	function row(el) {
 		if (el.hasClass('selected')) return;
-
-		// $('li.selected', $angrylib).animate({
-		// 	height: '48px'
-		// }, 250, function($) {
-		// 	el.removeClass('selected');
-
 		$('li.selected', $angrylib).removeClass('selected');
 		el.addClass('selected');
 	}
 
 	function big(el, cb) {
 		row(el.parent());
+
+		$('h2', el.parent()).animate({
+			left: el.css('left'),
+			top: '-50px'
+		}, 100);
+
+		el.css({ zIndex: 1 })
 		el.animate({
 			height: '200px',
 			width: '200px',
-			top: '20px'
 		}, 250, 'swing', cb)
 	}
 
 	function little(el, cb) {
 		el.animate({
 			height: '48px',
-			width: '48px',
-			top: '80px'
+			width: '48px'
 		}, 250, function() {
-
+			el.css({ zIndex: 0 })
 			var parent = el.parent();
-			// parent.remove(el);
 			parent.append(el);
 
 			if (cb) cb();
@@ -42,11 +40,7 @@
 	}
 
 	function next() {
-		if (idx >= images.length) {
-			idx = -1;
-		}
-
-		idx ++;
+		var idx = Math.floor(Math.random() * images.length);
 
 		big(images[idx], function() {
 			little(images[idx], next);
@@ -60,22 +54,33 @@
 		next();
 	}
 
+	var words = [];
+	var top = 0;
+	var left = 0;
 	
 	Util.HandleWordPreFill = function (Element, CurrentWord, WordCategory) {
 		var context = this;
 		if (!CurrentWord) return true;
-	
+
 		this.TwitterSearch(
 			Element, 
 			CurrentWord, 
 			function(el, word, response) {
 				var row;
 
-				if (!response || !response.results) {
+				if (words.indexOf(word) != -1 || words.length == 10 || !response || !response.results) {
 					return madLibEngine.fillNextWord(); 	
 				}
 
+				words.push(word);
+				
 				row = $('<li><h2>' + word + '?</h2></li>');
+				row.css({
+					top: top + 'px'
+				})
+
+				top += 50;
+
 
 				response.results.forEach(function(result) {
 	
@@ -83,7 +88,14 @@
 					row.append(img);
 					images.push(img);
 
+					img.css({
+						left: left + 'px'
+					})
+					left += 50;
+
 				});
+
+				left = 0;
 
 				$angrylib.append(row);
 
